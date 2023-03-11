176ee1f35e0c1ab299926c0d90d622ea90c16206
WojtekLap <wojciechlapunka@op.pl>
2023-03-11 10:39:52 +0100

    zadanie dz 4

---
+++ Adanie dzi 4/Program.cs
@@ -0,0 +1,19 @@
+﻿// zadnie dz 4
+string name = "Ewa";
+char sex = 'k';
+var age = 33;
+
+
+
+if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa 33 lata");
+}
+else if (sex == 'k' && age < 33)
+{
+    Console.WriteLine("kobieta ponizej 33 lat");
+}
+if (sex != 'k' && age < 18)
+{
+    Console.WriteLine("niepełnoletni mężczyzna");
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Zadanie domowe dzień 4
+using System.ComponentModel.Design;
+
+var Age = 33;
+string name = "Basia";
+char sex = 'k';
+
+if (sex == 'k' && Age < 33) ;
+{
+Console.WriteLine("kobieta ponizej 33 lat");
+}
+if (sex == 'k' && name == "Bsia") ;
+{
+Console.WriteLine("Basia lat 33");
+}
+if (sex != 'k' && Age != 33) ;
+{
+Console.WriteLine("mężczyzna niepełnoletni");
+}
+if (Age < 18 && sex == 'm') ;
+{
+Console.WriteLine("m niepełnoletni");
+}
+
+
+
