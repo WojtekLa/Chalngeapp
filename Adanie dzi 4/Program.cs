54d7902cf43b0023f2004c0519abc0b2edb138c1
WojtekLap <wojciechlapunka@op.pl>
2023-03-12 14:21:10 +0100

    zadanie dz 5

--- Adanie dzi 4/Program.cs
+++ Adanie dzi 4/Program.cs
@@ -1,19 +1,65 @@
-﻿// zadnie dz 4
-string name = "Ewa";
-char sex = 'k';
-var age = 33;
+﻿// zadanie dz 5 liczba 2482
 
 
+using System.Diagnostics.Metrics;
 
-if (name == "Ewa" && age == 33)
+int number = 2482;
+string numberAsstring = number.ToString();
+char[] letters = numberAsstring.ToArray();
+
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+
+foreach (char letter in letters) 
 {
-    Console.WriteLine("Ewa 33 lata");
-}
-else if (sex == 'k' && age < 33)
-{
-    Console.WriteLine("kobieta ponizej 33 lat");
-}
-if (sex != 'k' && age < 18)
-{
-    Console.WriteLine("niepełnoletni mężczyzna");
+    if (letter == '0')
+    {
+        counter0++;
+    }
+    else if (letter == '1') 
+    {
+        counter1++;
+    }
+    else if (letter =='2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        counter1++;
+    }
+    else if (letter == '5')
+    {
+        counter2++;
+    }
+    else if (letter == '6')
+    {
+        counter3++;
+    }
+    else if (letter == '7')
+    {
+        counter1++;
+    }
+    else if (letter == '8')
+    {
+        counter2++;
+    }
+    else if (letter == '9')
+    {
+        counter3++;
+    }
+
 }
