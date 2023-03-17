4184690b81cfb7f597458b0ef047077ac6b215df
WojtekLap <wojciechlapunka@op.pl>
2023-03-17 09:01:35 +0100

    zadanie dz4

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+var name = "Ewa";
+bool female = false;
+var age = 17;
+
+
+if (name == "Ewa" && age == 33 && female)
+{
+    Console.WriteLine("Ewa lat 33");
+}
+else if (!female && age < 18)
+{
+Console.WriteLine("niepe lnoletni mężczyzna");
+}
+else if (female && age < 33)
+{
+    Console.WriteLine("kobieta ponizej  33 lat");
+}
